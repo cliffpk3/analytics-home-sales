a9b7c9d5fde6bfebdd71bab80f04f1db0d4de028
cliffpk3 <carli.ricardo@gmail.com>
2022-02-19 08:11:59 -0300

    19/02/2022

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pandas as pd
 import plotly.express as px
 import folium
 import geopandas
+from millify import millify
+import matplotlib.pyplot as plt
 from streamlit_folium import folium_static
 from folium.plugins import MarkerCluster
 
@@ -12,6 +14,7 @@ pd.set_option('display.float_format', lambda x: '%.2f' % x)
 st.set_page_config(layout='wide')
 path = 'reworked_data.csv'
 geopath = 'https://opendata.arcgis.com/datasets/83fc2e72903343aabff6de8cb445b81c_2.geojson'
+counter = 0
 
 @st.cache(allow_output_mutation=True)
 def get_data(path):
@@ -23,25 +26,17 @@ def get_geodata(geopath):
     geofile = geopandas.read_file(geopath)
     return geofile
 
-def data_filters(df):
-    # 1 Data Filters
-    st.sidebar.title('General Filters')
-    st.sidebar.header('Dataframe Filters')
-
-    # 1.1 Columns
-    f_columns = st.sidebar.multiselect('Specify columns', df.columns)
-    if f_columns == []:
-        f_columns = df.columns
-
-    f_dataframe = st.checkbox('Show source dataframe')
-    if f_dataframe:
-        st.write(df[f_columns].head())
-
-    return None
+def sample(df):
+    f_fulldata = st.checkbox('Use full data sample')
+    if f_fulldata == False:
+        df = df.sample(100, random_state=101)
+    else:
+        df = df
+    return df
 
 def map_filters(df):
     # 2 Map Filters
-    st.sidebar.title('Map Filters')
+    st.sidebar.title('Map Filters :world_map:')
 
     # 2.1 Bedrooms
     f_bedrooms = st.sidebar.multiselect('Number of Bedrooms',
@@ -62,7 +57,7 @@ def map_filters(df):
         f_floors = list(sorted(df['floors'].unique()))
 
     # 2.4 Waterfront
-    f_waterfront = st.sidebar.checkbox('Only waterfront properties')
+    f_waterfront = st.sidebar.checkbox(f'Only waterfront properties')
     if f_waterfront:
         f2_waterfront = [1]
     else:
@@ -88,94 +83,107 @@ def map_filters(df):
         (df['waterfront'].isin(list(set(df['waterfront']).intersection(f2_waterfront)))) &
         (df['worth_buying'].isin(list(set(df['worth_buying']).intersection(f2_worthbuying))))
         ][['id', 'date', 'lat', 'long', 'price', 'sqft_living', 'bedrooms', 'bathrooms', 'yr_built', 'zipcode',
-           'worth_buying',  'profit_value', 'selling_price']]  # ][['id', 'lat', 'long', 'price']]
-
-    #FOR FASTER TESTING:
-    if len(houses) >= 100:
-        houses = houses.sample(100, random_state=101)
+           'worth_buying',  'profit_value', 'selling_price', 'condition']]  # ][['id', 'lat', 'long', 'price']]
 
     return houses
 
 def charts(houses):
-    fig = px.scatter(houses, x='price', y='id', height=400, color='worth_buying', color_continuous_scale=['#da0721', '#60cd06'],
-                     size='price', size_max=20,
-                     title='<b>Worth Buying Properties</b>',
-                     hover_name='id', hover_data={'price': ':.1f', 'worth_buying': ':.f', 'profit_value': ':.2f'},
-                     labels={'price': 'Price', 'worth_buying': 'Worth Buying', 'profit_value': 'Profit Value'})
-    fig.update_layout(title_x=0.5, margin=dict(l=10, r=10, t=25, b=10))
-    fig.update_coloraxes(showscale=False)
-    st.plotly_chart(fig, use_container_width=True)
 
+    st.subheader('#1) Which real states must be bought by Home Sales and for how much?')
+    x1, x2 = st.columns([4,1])
+    with x1:
+        fig = px.scatter(houses, x='price', y='yr_built', title='<b>Worth Buying Properties</b>',
+                         height=300, color='worth_buying', color_continuous_scale=['#EF553B', '#00CC96'], opacity=0.9,
+                         size='price', size_max=20,
+                         hover_name='id',
+                         hover_data={'price': ':,.2f', 'worth_buying': ':.f', 'yr_built':':.f'},
+                         labels={'price': 'Price', 'worth_buying': 'Worth Buying', 'yr_built':'Year Built'},
+                         template='ggplot2')
+        fig.update_layout(title_x=0.525, margin=dict(l=0, r=0, t=25, b=0))
+        fig.update_yaxes(showticklabels=False, showgrid=True, title="Year Built", ticks="") #visible=False
+        fig.update_xaxes(showgrid=True, ticks="")  # visible=False
+        fig.update_coloraxes(showscale=False)
+        st.plotly_chart(fig, use_container_width=True)
+    with x2:
+        st.write('  \n')
+        st.write('  \n')
+        st.write('  \n')
+        st.metric(label='Worth Buying', value=f'{houses[houses["worth_buying"] == 1]["worth_buying"].count()}/{houses["worth_buying"].count()}', delta=f'{(houses[houses["worth_buying"] == 1]["worth_buying"].count()/houses["worth_buying"].count())*100:.2f}% worths!')
+        st.metric(label='Total Investment x Profit Values', value=f'U${millify(houses[houses["worth_buying"] == 1]["price"].sum(), precision="2")}', delta=f'U${millify(houses[houses["worth_buying"] == 1]["profit_value"].sum(), precision=2)} profit!')
+
+    st.subheader('#2) Once the real state is bought, for how much it should be sold?')
     houses2 = houses[houses['worth_buying'] == 1]
-    fig2 = px.scatter(houses2, x='price', y='id', height=400, size='profit_value', size_max=20,
-                      color_discrete_sequence=['#FF5733'],
-                      title='<b>Price x Profit</b>',
-                      hover_name='id', hover_data={'price': ':.1f', 'profit_value': ':.2f'},
-                      labels={'price': 'Price', 'profit_value': 'Profit Value'})
-    fig2.update_layout(title_x=0.5, margin=dict(l=10, r=10, t=25, b=10))
+    fig2 = px.scatter(houses2, x='selling_price', y='yr_built', title='<b>Selling Price x Profit</b>',
+                      height=300, color_discrete_sequence=['#AB63FA'], opacity=0.9,
+                      size='profit_value', size_max=20,
+                      hover_name='id',
+                      hover_data={'selling_price': ':,.2f', 'profit_value': ':.2f'},
+                      labels={'selling_price': 'Selling Price', 'profit_value':'Profit Value','yr_built':'Year Built'})
+    fig2.update_layout(title_x=0.525, margin=dict(l=0, r=0, t=25, b=0))
+    fig2.update_yaxes(showticklabels=False)
     fig2.update_coloraxes(showscale=False)
     st.plotly_chart(fig2, use_container_width=True)
 
 def maps(geofile, houses):
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True) #Exibir os checkboxes na horizontal
     map_type = ['Price Map', 'Density Map', 'Region Price Map']
-    map = st.radio('Map type', map_type, index=1)
+    map = st.radio('Select map type to show:', map_type)
     #Price Map
-    price_map = px.scatter_mapbox(houses, lat='lat', lon='long', size='price', color_continuous_scale=['green','red'], size_max=10, zoom=10, color='price', hover_name='id')
+    price_map = px.scatter_mapbox(houses, lat='lat', lon='long', size='price', color_continuous_scale=['#60cd06','#da0721'], size_max=12, zoom=10, color='price', hover_name='id')
     price_map.update_layout(mapbox_style='open-street-map', height=800, margin={'r': 0, 't': 0, "l": 0, "b": 0})
     #Density Map
     density_map = folium.Map(location=[houses['lat'].mean(), houses['long'].mean() ], default_zoom_start=15 )
     marker_cluster = MarkerCluster().add_to(density_map)
-    popitup = folium.Popup("test", max_width=500, min_width=600, show=True)
+    popitup = folium.Popup("test", max_width=800, min_width=800, show=True)
     for name, row in houses.iterrows():
        folium.Marker([row['lat'], row['long']],
            popup='<i>${:,.2f}</i>.'
                  '<br><b>Sqft:</b> {};'
                  '<br><b>Bedroom:</b> {};'
                  '<br><b>Bathroom:</b> {:,.0f};'
-                 '<br><b>Year Built:</b> {}.'.format(
-                 row['price'],
-                 row['sqft_living'],
-                 row['bedrooms'],
-                 row['bathrooms'],
-                 row['yr_built']).replace(",",".")).add_to(marker_cluster)
+                 '<br><b>Year Built:</b> {}.'.format(row['price'], row['sqft_living'], row['bedrooms'], row['bathrooms'], row['yr_built']).replace(",",".")).add_to(marker_cluster)
+
     #Region Price Map
     df2 = houses[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
     df2.columns = ['ZIP', 'PRICE']
     geofile = geofile[geofile['ZIP'].isin(df2['ZIP'].tolist())]
     region_price_map = folium.Map(location=[df['lat'].mean(), df['long'].mean()], default_zoom_start=15)
-    region_price_map.choropleth(data=df2,
-                               geo_data=geofile,
-                               columns=['ZIP', 'PRICE'],
-                               key_on='feature.properties.ZIP',
-                               fill_color='YlOrRd',
-                               fill_opacity=0.7,
-                               line_opacity=0.2,
-                               legend_name='AVG PRICE')
+    region_price_map.choropleth(data=df2, geo_data=geofile, columns=['ZIP', 'PRICE'], key_on='feature.properties.ZIP', fill_color='YlOrRd', fill_opacity=0.7, line_opacity=0.2, legend_name='Average price')
+
     if map == 'Price Map':
-        st.plotly_chart(price_map, use_container_width=True)
+            st.plotly_chart(price_map, use_container_width=True)
     elif map == 'Density Map':
-        folium_static(density_map)
+            folium_static(density_map)
     else:
-        folium_static(region_price_map)
+            folium_static(region_price_map)
 
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+def counter():
+    st.session_state.count += 1
+
+def intro():
+    st.write('Home Sales is a company specialized in buying and reselling real states.  \n'
+             'Our business model consists of analyzing features and locations of properties to perform '
+             'advantageous buying and selling operations.  \n'
+             'Currently, the objective of data analysis is to find and filter those properties with safe '
+             'purchase and profit potential through the answer of two business questions:  \n\n'
+             '__Question 1) Which real states must be bought by Home Sales and for how much?__  \n'
+             '__Question 2) Once the real state is bought, for how much it should be sold?__  \n\n'
+            'The estimated profit (profit_value) on the purchase of a property is about 30% of its purchase value.  \n'
+            'This application allows you to travel beyond these data and properties, choosing and filtering its features, locations and values.')
+    st.button('Show me the data!', on_click=counter)
+
+st.title('Home Sales Data Overview :cityscape:')
 if __name__ == '__main__':
-    df = get_data(path)
-    geofile = get_geodata(geopath)
-    data_filters(df)
-    houses = map_filters(df)
-    charts(houses)
-    maps(geofile, houses)
-
-#st.title('Home Sales Company')
-#st.write('A Home Sales é uma imobiliária fictícia que trabalha com a compra e venda de imóveis. '
-#         'Buscamos, através da análise de características e dados dos imóveis, alcançar insights que possibilitam  aquisições de imóveis mais vantajosas e seguras.  \n'
-#         'O objetivo desse projeto é relatar a análise de um conjunto de dados de imóveis e, por fim, destacar  aqueles que são interessantes para compra.')
-#
-#st.header('Questões de Negócio:')
-#st.write('1) Quais imóveis a Home Sales deve comprar e por qual preço?')
-#st.write('Os critérios de compra são:  \na) Imóveis abaixo da mediana de preço daquela região;  \nb) Em boas condições;  \n c) Em que é possível um ganho de pelo menos 10% sobre o valor de compra.')
-#st.write('Os imóveis da amostra que cumprem esses requisitos são listados no gráfico abaixo:')
-#
-#
-#st.write('2) Uma vez que o imóvel foi adquirido, qual deve ser seu valor de venda?')
-#st.write('Os imóveis da amostra são exibidos no gráfico abaixo, o tamanho do círculo reflete o percentual de lucro em relação ao valor de compra do imóvel no processo aquisição.')
+    if st.session_state.count % 2 == 0:
+        intro()
+    if st.session_state.count % 2 != 0:
+        df = get_data(path)
+        geofile = get_geodata(geopath)
+        df = sample(df)
+        houses = map_filters(df)
+        charts(houses)
+        maps(geofile, houses)
+        st.sidebar.caption('[_Contact me. :email: :flag-br:_](https://www.linkedin.com/in/ricardo-estevam-carli-475461181/)')
